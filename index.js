e782074932548de3a6d88aa2d2524451c9c58639
MrBl4ck <franz.carvajal04@gmail.com>
2024-03-02 01:46:17 -0400

    Se agrego la funcionalidad de los likes

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const scroll = document.getElementById("scroll");
 scroll.addEventListener('click', () => {
     document.querySelector(".get-started").scrollIntoView({ behavior: 'smooth' });
 })
+
+// Selecciona el corazón y el contador de likes por sus IDs
+const likeHeart = document.getElementById("likeHeart");
+const likesCounter = document.getElementById("likesCounter");
+
+// Añade un evento de clic al corazón
+likeHeart.addEventListener("click", function() {
+  // Incrementa el contador de likes
+  let currentLikes = parseInt(likesCounter.innerText);
+  likesCounter.innerText = currentLikes + 1;
+});
