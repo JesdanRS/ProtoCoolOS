393d2b4ce5cc3f91972185fefbbf4469a6cea388
luwu <lucivp22@gmail.com>
2024-04-06 20:46:00 -0400

    Inicializar gráfico en 0

--- SelectionSort/sortback.js
+++ SelectionSort/sortback.js
@@ -46,36 +46,12 @@ document.addEventListener('DOMContentLoaded', function() {
         resultadoContainer.style.display = 'block'; // Muestra el contenedor con los resultados
     }
 
-    async function visualizarSelectionSort(arr) {
-        let startTime = performance.now(); // Captura el tiempo de inicio
-    
-        for (let i = 0; i < arr.length - 1; i++) {
-            let min = i;
-            for (let j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
-                dibujarGraficoBarras(arr, [min, j]);
-                await sleep(100); // Pausa para visualización
-            }
-            if (min !== i) {
-                [arr[i], arr[min]] = [arr[min], arr[i]];
-                dibujarGraficoBarras(arr, [i, min]);
-                await sleep(100); // Pausa para visualización
-            }
-        }
-    
-        let endTime = performance.now(); // Captura el tiempo de finalización
-        let tiempoOrdenamiento = (endTime - startTime) / 1000; // Calcula la diferencia y convierte a segundos
-        document.getElementById('tiempoOrdenamiento').textContent = tiempoOrdenamiento.toFixed(2); // Muestra el tiempo en el elemento del DOM
-        document.getElementById('listaOrdenada').textContent = arr.join(", ");
-    }
-
     function dibujarGraficoBarras(datos, highlightIndexes = []) {
         const ctx = document.getElementById('graficoBarras').getContext('2d');
         if (miGrafico) {
             miGrafico.destroy();
         }
+        
         miGrafico = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -122,84 +98,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
-    
-    /*function dibujarGraficoBarras(datos, highlightIndexes = []) {
-        const svg = d3.select('#graficoBarrasContainer svg');
-        svg.selectAll("*").remove();
-    
-        const margin = {top: 20, right: 20, bottom: 30, left: 40},
-              width = +svg.attr("width") - margin.left - margin.right,
-              height = +svg.attr("height") - margin.top - margin.bottom;
-    
-        const x = d3.scaleBand()
-                    .range([0, width])
-                    .padding(0.1)
-                    .domain(datos.map((_, i) => i));
-    
-        const y = d3.scaleLinear()
-                    .range([height, 0])
-                    .domain([0, d3.max(datos, d => d)]);
-    
-        const g = svg.append("g")
-                     .attr("transform", `translate(${margin.left},${margin.top})`);
-    
-        g.selectAll(".bar")
-         .data(datos)
-         .enter().append("rect")
-           .attr("class", "bar")
-           .attr("x", (d, i) => x(i))
-           .attr("y", d => y(d))
-           .attr("width", x.bandwidth())
-           .attr("height", d => height - y(d))
-           .attr("fill", (_, i) => highlightIndexes.includes(i) ? '#581790' : '#3b82f6')
-           .on("mouseover", function(event, d) {
-               const [x, y] = d3.pointer(event, svg.node());
-    
-               // Crear un grupo para el tooltip
-               const tooltip = svg.append("g")
-                                  .attr("id", "tooltip")
-                                  .style("pointer-events", "none");
-    
-               // Agregar un rectángulo gris como fondo
-               tooltip.append("rect")
-                      .attr("x", x)
-                      .attr("y", y - 30)
-                      .attr("width", 60)
-                      .attr("height", 20)
-                      .attr("fill", "grey")
-                      .attr("rx", 4) // bordes redondeados
-                      .style("alignment-baseline", "middle");
-    
-               // Agregar texto mostrando el valor de la barra
-               tooltip.append("text")
-                      .attr("x", x + 30)
-                      .attr("y", y - 15)
-                      .attr("fill", "white")
-                      .attr("text-anchor", "middle")
-                      .text(d);
-           })
-           .on("mouseout", function() {
-               svg.select("#tooltip").remove();
-           });
-    
-        // Agregando ejes y cambiando su color a blanco
-        g.append("g")
-         .attr("transform", `translate(0,${height})`)
-         .call(d3.axisBottom(x).tickFormat(i => i + 1))
-         .selectAll("text")
-         .style("color", "white");
-    
-        g.append("g")
-         .call(d3.axisLeft(y))
-         .selectAll("text")
-         .style("color", "white");
-    
-        // Cambiar el color de las líneas de los ejes a blanco
-        svg.selectAll("path.domain")
-           .style("stroke", "white");
-        svg.selectAll(".tick line")
-           .style("stroke", "white");
-    }*/    
 
     agregarListaBtn.addEventListener('click', function() {
         let cantidad = prompt('¿Cuántos números deseas agregar a la lista?');
@@ -269,4 +167,6 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('listaOriginal').textContent = ""; // Limpia la lista original
         document.getElementById('listaOrdenada').textContent = ""; 
     });
+
+    dibujarGraficoBarras(listaNumeros);
 });
